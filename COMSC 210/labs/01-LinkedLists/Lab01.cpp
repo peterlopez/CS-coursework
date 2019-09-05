fbcbcb122356d8f4c6d77055509f27e05ad242ae
Peter Ruszel <peter@phpete.me>
2019-09-05 14:43:26 -0700

    updated lab1

--- COMSC 210/labs/01-LinkedLists/Lab01.cpp
+++ COMSC 210/labs/01-LinkedLists/Lab01.cpp
@@ -15,7 +15,7 @@ public:
     string getArtist();
 };
 
-void addSongToPlaylist(vector<Song>&, Song);
+void addSongToPlaylist(vector<Song>&, Song, int);
 void deleteSongFromPlaylist(vector<Song>&, int);
 void displayPlaylist(vector<Song>&);
 void playlistMenu();
@@ -50,8 +50,9 @@ void playlistMenu()
     // options for user
     const int CODE_ADD = 1;
     const int CODE_DELETE = 2;
-    const int CODE_DISPLAY = 3;
-    const int CODE_EXIT = 4;
+    const int CODE_REORDER = 3;
+    const int CODE_DISPLAY = 4;
+    const int CODE_EXIT = 5;
     // selection by user
     int choice = 0;
     // used to display result of last operation
@@ -66,6 +67,7 @@ void playlistMenu()
         cout << "Playlist menu" << endl;
         cout << "(" << CODE_ADD << ") Add a song to the playlist" << endl;
         cout << "(" << CODE_DELETE << ") Delete a song from the playlist" << endl;
+        cout << "(" << CODE_REORDER << ") Reorder songs" << endl;
         cout << "(" << CODE_DISPLAY << ") Display the playlist" << endl;
         cout << "(" << CODE_EXIT << ") Exit" << endl << endl;
 
@@ -84,9 +86,16 @@ void playlistMenu()
                 getline(cin, newSongName);
                 cout << "Enter song artist: ";
                 getline(cin, newSongArtist);
+                int pos = 0;
+                if (playlist.size() > 0) {
+                    cout << "Enter position in playlist (1-" << playlist.size()+1 << "): ";
+                    cin >> buf;
+                    pos = atoi(buf.c_str()) - 1; // user-friendly index (starts at 1)
+                    cout << endl;
+                }
 
                 Song newSong(newSongName, newSongArtist);
-                addSongToPlaylist(playlist, newSong);
+                addSongToPlaylist(playlist, newSong, pos);
 
                 outputMsg = "New song added!";
                 break;
@@ -100,17 +109,62 @@ void playlistMenu()
                 displayPlaylist(playlist);
                 cout << endl << "-------------" << endl;
 
+                bool inputIsInvalid;
                 int songNumber; // user-friendly index (starts at 1)
                 do {
                     cout << "Enter song # to delete: ";
                     cin >> buf;
                     songNumber = atoi(buf.c_str());
                     cout << endl;
-                } while(songNumber <= 0 || songNumber > playlist.size());
+                    inputIsInvalid = songNumber <= 0 || songNumber > playlist.size();
+                    if (inputIsInvalid) {
+                        cout << "Error: invalid input" << endl;
+                    }
+                } while(inputIsInvalid);
 
                 deleteSongFromPlaylist(playlist, songNumber - 1);
 
                 outputMsg = "Song #" + to_string(songNumber) + " deleted.";
+                break;
+            }
+            case CODE_REORDER: {
+                if (playlist.size() < 2) {
+                    outputMsg = "Not enough songs in playlist.";
+                    break;
+                }
+                system("CLEAR");
+                displayPlaylist(playlist);
+
+                bool inputIsInvalid;
+                int songNumber; // user-friendly index (starts at 1)
+                do {
+                    cout << "Enter song # to move: ";
+                    cin >> buf;
+                    songNumber = atoi(buf.c_str());
+                    cout << endl;
+                    inputIsInvalid = songNumber <= 0 || songNumber > playlist.size();
+                    if (inputIsInvalid) {
+                        cout << "Error: invalid input" << endl;
+                    }
+                } while(inputIsInvalid);
+                int destNumber; // user-friendly index (starts at 1)
+                do {
+                    cout << "Enter desired # position (1-" << playlist.size() << "): ";
+                    cin >> buf;
+                    destNumber = atoi(buf.c_str());
+                    cout << endl;
+                    inputIsInvalid = destNumber <= 0 || destNumber > playlist.size();
+                    if (inputIsInvalid) {
+                        cout << "Error: invalid input" << endl;
+                    }
+                } while (inputIsInvalid);
+
+                Song targetSong = playlist.at(songNumber-1);
+                playlist.erase(playlist.begin() + songNumber-1);
+                playlist.insert(playlist.begin() + destNumber-1, targetSong);
+
+                outputMsg = "Song moved!";
+
                 break;
             }
             case CODE_DISPLAY: {
@@ -134,9 +188,9 @@ void playlistMenu()
     }
 }
 
-void addSongToPlaylist(vector<Song>& playlist, Song newSong)
+void addSongToPlaylist(vector<Song>& playlist, Song newSong, int pos)
 {
-    playlist.push_back(newSong);
+    playlist.insert(playlist.begin() + pos, newSong);
 }
 
 void deleteSongFromPlaylist(vector<Song>& playlist, int songIndex)
