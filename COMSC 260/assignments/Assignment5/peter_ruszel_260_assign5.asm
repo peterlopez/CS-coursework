b21617496738adf8b147a9e3242f4fc15d9a87e1
Peter Lopez <peter@phpete.me>
2019-10-01 17:00:44 -0700

    finish assign5

--- COMSC 260/assignments/Assignment5/peter_ruszel_260_assign5.asm
+++ COMSC 260/assignments/Assignment5/peter_ruszel_260_assign5.asm
@@ -12,6 +12,84 @@ ExitProcess proto,dwExitCode:dword
 .code
 main proc
 	
+	; Phase 1
+	;---------------------
+	mov AH, var1
+	mov AL, var2
+	mov var2, AH
+	mov AH, var3
+	mov var3, AL
+	mov AL, var4
+	mov var4, AH
+	mov var1, AL
+
+	; reset EAX
+	mov eax, 0
+
+
+
+	; Phase 2
+	;---------------------
+	; move first two bytes into lower 16-bit registers of EAX
+	mov AH, var1
+	mov AL, var2
+
+	; shift first two bytes into upper 16-bits of EAX
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	add eax, eax
+	
+	; move last two bytes into lower 16-bit registers of EAX
+	mov AH, var3
+	mov AL, var4
+
+
+
+	; Phase 3
+	;---------------------
+	; reset EBX
+	mov ebx, 0
+	
+	; get two's complement of first two bytes
+	sub bh, var1
+	sub bl, var2
+
+	; shift first two bytes into upper 16-bits of EBX
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+	add ebx, ebx
+
+	; get two's complement of last two bytes
+	; and move into lower 16-bit registers of EBX
+	sub bh, var3
+	sub bl, var4
+
 	invoke ExitProcess,0
 main endp
 end main
